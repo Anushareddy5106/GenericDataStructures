41f06917df88d69017a4edec1894594f771bd8ec
Anushareddy5106 <anushachandupatla.2003@gmail.com>
2023-12-26 21:08:52 +0530

    Pop method added

--- src/App.java
+++ src/App.java
@@ -2,21 +2,33 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("LinkedList Using Generics");
 
+        // LinkedList Creation
         LinkedList myLinkedList1 = new LinkedList();
 
         myLinkedList1.append(56);
         myLinkedList1.append(30);
         myLinkedList1.append(70);
 
+        // Insertion
         LinkedList myLinkedList2 = new LinkedList();
 
         myLinkedList2.append(56);
         myLinkedList2.append(70);
 
         Node prevNode = myLinkedList2.head;
+        System.out.println("Before Insertion");
+        myLinkedList2.display();
 
         myLinkedList2.insertAfter(prevNode, 30);
 
+        System.out.println("After Insertion");
         myLinkedList2.display();
+
+        // deletion
+        myLinkedList2.pop();
+
+        System.out.println("After Deletion");
+        myLinkedList2.display();
+
     }
 }
--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,6 +1,7 @@
 public class LinkedList {
     Node head;
 
+    // Adding the element
     public void append(int data) {
         Node newNode = new Node(data);
         if (head == null) {
@@ -14,6 +15,7 @@ public class LinkedList {
         }
     }
 
+    // Insertion
     public void insertAfter(Node prevNode, int data) {
         if (prevNode == null) {
             System.out.println("Previous node cannot be null.");
@@ -23,6 +25,16 @@ public class LinkedList {
         Node newNode = new Node(data);
         newNode.next = prevNode.next;
         prevNode.next = newNode;
+    }
+
+    // Delete from front
+    public void pop() {
+        if (head == null) {
+            System.out.println("The linked list is empty. Cannot pop.");
+            return;
+        }
+
+        head = head.next;
     }
 
     public void display() {
